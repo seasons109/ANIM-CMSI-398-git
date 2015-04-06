cd51017395ad6b821bb9a7d35f84e8d993828335
unknown <sshigeta@ANM-CA301-PC03.lmumain.edu>
2015-04-06 10:26:03 -0700

    Week 13 initial

---
+++ sshigeta_anim398_01_week13_envVarFOR_v1.py
@@ -0,0 +1,8 @@
+#Dictionary In-Class Assignment
+env_var = {}
+env_var ['$HIP'] = 'the home directory'
+env_var ['$JOB'] = 'the project directory'
+
+#use hou.expandString('name of the file to figure it out')
+
+for
