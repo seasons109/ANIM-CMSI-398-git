790adba576679188db265c08aaeb73bfb0233dfa
Summer <sshigeta@lion.lmu.edu>
2015-04-08 00:42:49 -0700

    Parses through value's letters and checks to see if letter matches

--- sshigeta_anim398_01_week13_envVarFOR_v1.py
+++ sshigeta_anim398_01_week13_envVarFOR_v1.py
@@ -9,13 +9,22 @@ env_var ['$HDA'] = 'HDA file path'
 env_var ['$HOME'] = 'HOME file path'
 
 #Asks for prompt of what they would like the letter to search for the variable.
-searched_letter = 'r'
+searched_letter = 'i'
+contains_letter_list = []
 
 for env_keys in env_var: 
-	
+	contains_letter = False
+
 	for letter in range( len(env_var[env_keys]) ):
 		if searched_letter == env_var[env_keys][letter]:
-			print("It worked! " + env_var[env_keys] )
+			contains_letter = True
 
-		print("letter: " + str(letter) + "   " +  env_var[env_keys][letter])
+		#print("letter: " + str(letter) + "   " +  env_var[env_keys][letter])
+	if contains_letter: 
+		contains_letter_list.append( env_keys )
+	print( env_keys + " is mapped to " + env_var[env_keys] )
 
+if (len(contains_letter_list) == 0 ):
+    print("No keys contain the letter " + searched_letter )
+else: 
+    print(str(contains_letter_list) + " contain(s) the letter: " + searched_letter );
